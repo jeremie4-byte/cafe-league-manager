ec06aeb3000ef017c822a53c4a40aed638e0da4d
jeremie4-byte <lortiejeremie@gmail.com>
2026-07-14 21:26:32 +0200

    Add EloCalculation class with win probability method

--- domains/elo.py
+++ domains/elo.py
@@ -1,1 +1,8 @@
+K_FACTOR = 30
 
+class EloCalculation:
+    @staticmethod
+    def probabilistic_elo(rating_a, rating_b):
+        win_probabilityA =1.0 / (1 + (10**((rating_b - rating_a)/400)))
+        win_probabilityB = 1 - win_probabilityA
+        return (win_probabilityA, win_probabilityB)
