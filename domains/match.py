9f8340b45558db908de7504d06370574162261df
jeremie4-byte <lortiejeremie@gmail.com>
2026-07-20 19:12:37 +0200

    Implement validation in Match class constructor
    Add validation for match_id, event_id, and schedule_match_time in Match class.

--- domains/match.py
+++ domains/match.py
@@ -4,5 +4,11 @@ from datetime import datetime
 class Match:
     def __init__(self, match_id, event_id, schedule_match_time: datetime):
         self.match_id = match_id
+        if match_id < 0:
+            raise ValueError
         self.event_id = event_id
+        if event_id < 0:
+            raise ValueError
         self.schedule_match_time = schedule_match_time
+        if not isinstance(schedule_match_time, datetime):
+            raise ValueError
