d5796578150406d4e366fd625669f74ca85af3f2
jeremie4-byte <lortiejeremie@gmail.com>
2026-07-22 19:59:45 +0200

    Implement validation for Event class attributes
    Added validation checks for event attributes in the Event class.

--- domains/event.py
+++ domains/event.py
@@ -11,8 +11,20 @@ class EventType(Enum):
 class Event:
     def __init__(self, event_id, event_name, event_type: EventType, game_title, event_capacity, event_date: datetime):
         self.event_id = event_id
+        if event_id < 0:
+            raise ValueError
         self.event_name = event_name
+        if event_name == "":
+            raise ValueError
         self.event_type = event_type
+        if event_type != "Open Game Night" and event_type != "Ranked League Match" and event_type != "Tournament Match":
+            raise ValueError
         self.game_title = game_title
+        if game_title == "":
+            raise ValueError
         self.event_capacity = event_capacity
+        if event_capacity < 1 or event_capacity > 100:
+            raise ValueError
         self.event_date = event_date
+        if not isinstance(event_date, datetime):
+            raise ValueError
