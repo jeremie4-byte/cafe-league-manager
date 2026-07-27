1b0491d7f8bb5e55224b080f8083623709ab61d8
jeremie4-byte <lortiejeremie@gmail.com>
2026-07-27 21:03:10 +0200

    Refactor Event class to use validation functions

--- domains/event.py
+++ domains/event.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from datetime import datetime
+from services.validators import *
 
 """Creating an Enum class for all different SQL event_type"""
 class EventType(Enum):
@@ -10,21 +11,11 @@ class EventType(Enum):
 """Creating Event class with all SQL attributes"""
 class Event:
     def __init__(self, event_id, event_name, event_type: EventType, game_title, event_capacity, event_date: datetime):
-        self.event_id = event_id
-        if event_id < 0:
-            raise ValueError
-        self.event_name = event_name
-        if event_name == "":
-            raise ValueError
-        self.event_type = event_type
+        self.event_id = validate_positive_id(event_id)
+        self.event_name = validate_min_name_length(event_name)
         if not isinstance(event_type, EventType):
-            raise ValueError
-        self.game_title = game_title
-        if game_title == "":
-            raise ValueError
-        self.event_capacity = event_capacity
-        if event_capacity < 1 or event_capacity > 100:
-            raise ValueError
-        self.event_date = event_date
-        if not isinstance(event_date, datetime):
-            raise ValueError
+            raise TypeError("event_type must be a valid EventType enum instance")
+        self.event_type = event_type
+        self.game_title = validate_min_name_length(game_title)
+        self.event_capacity = validate_event_capacity(event_capacity)
+        self.event_date = validate_datetime_format(event_date)
