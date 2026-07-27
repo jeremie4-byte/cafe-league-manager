bbda7ff66687e1ca86821a903d0557c3fb954bf5
jeremie4-byte <lortiejeremie@gmail.com>
2026-07-27 21:02:44 +0200

    Refactor Match class initialization with validators
    Validator logic added to domain class

--- domains/match.py
+++ domains/match.py
@@ -1,14 +1,9 @@
 from datetime import datetime
+from services.validators import *
 
 """Creating Match class with all SQL attributes"""
 class Match:
     def __init__(self, match_id, event_id, schedule_match_time: datetime):
-        self.match_id = match_id
-        if match_id < 0:
-            raise ValueError
-        self.event_id = event_id
-        if event_id < 0:
-            raise ValueError
-        self.schedule_match_time = schedule_match_time
-        if not isinstance(schedule_match_time, datetime):
-            raise ValueError
+        self.match_id =  validate_positive_id(match_id)
+        self.event_id =  validate_positive_id(event_id)
+        self.schedule_match_time = validate_datetime_format(schedule_match_time)
