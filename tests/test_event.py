78e0aa29e0edae1a91c07503b28f477880fefa38
jeremie4-byte <lortiejeremie@gmail.com>
2026-07-27 18:16:14 +0200

    Refactor event tests to use EventType constants

--- tests/test_event.py
+++ tests/test_event.py
@@ -7,13 +7,13 @@ class TestEvent(unittest.TestCase):
 
     """Default set up case for testing"""
     def setUp(self):
-        self.event = Event(1, "Brothers in Mario Bros", "Open Game Night", "Mario Bros", 50, datetime(2026, 7, 22))
+        self.event = Event(1, "Brothers in Mario Bros", EventType.OPEN_GAME_NIGHT, "Mario Bros", 50, datetime(2026, 7, 22))
 
     """Default event test case for event"""
     def test_default_case(self):
         self.assertEqual(self.event.event_id, 1)
         self.assertEqual(self.event.event_name, "Brothers in Mario Bros")
-        self.assertEqual(self.event.event_type, "Open Game Night")
+        self.assertEqual(self.event.event_type, EventType.OPEN_GAME_NIGHT)
         self.assertEqual(self.event.game_title, "Mario Bros")
         self.assertEqual(self.event.event_capacity, 50)
         self.assertEqual(self.event.event_date, datetime(2026, 7, 22))
@@ -21,41 +21,42 @@ class TestEvent(unittest.TestCase):
     """Test invalid event_id below 1"""
     def test_invalid_event_id(self):
         with self.assertRaises(ValueError):
-            event2 = Event(-5, "Chaining Links", "Ranked League Match", "The Legend of Zelda: Ocarina of Time", 20, datetime(2026, 6, 20))
+            event2 = Event(-5, "Chaining Links", EventType.RANKED_LEAGUE_MATCH, "The Legend of Zelda: Ocarina of Time", 20, datetime(2026, 6, 20))
 
     """Test valid 1 ID"""
     def test_valid_1event_id(self):
-        event3 = Event(1, "Bombless Bomber Man", "Tournament Match", "Bomberman", 35, datetime(2025, 12, 12))
+        event3 = Event(1, "Bombless Bomber Man", EventType.TOURNAMENT_MATCH, "Bomberman", 35, datetime(2025, 12, 12))
         self.assertEqual(event3.event_id, 1)
 
     """Test valid event ID"""
     def test_valid_event_id(self):
-        event4 = Event(2, "Pokemon Red: Nuzlock", "Ranked League Match", "Pokemon: Red Edition", 15, datetime(2026, 1, 5))
+        event4 = Event(2, "Pokemon Red: Nuzlock", EventType.RANKED_LEAGUE_MATCH, "Pokemon: Red Edition", 15, datetime(2026, 1, 5))
         self.assertEqual(event4.event_id, 2)
 
     """Test invalid event name"""
     def test_invalid_event_name(self):
         with self.assertRaises(ValueError):
-            event5 = Event(3, "", "Open Game Night", "Mario Bros", 23, datetime(2026, 2, 27))
+            event5 = Event(3, "", EventType.OPEN_GAME_NIGHT, "Mario Bros", 23, datetime(2026, 2, 27))
 
     """Test valid event name"""
     def test_valid_event_name(self):
-        event6 = Event(4, "BLJ sequence break", "Ranked League Match", "Mario 64", 20, datetime(2025, 11, 11))
+        event6 = Event(4, "BLJ sequence break", EventType.RANKED_LEAGUE_MATCH, "Mario 64", 20, datetime(2025, 11, 11))
         self.assertEqual(event6.event_name, "BLJ sequence break")
 
     """Test valid Open Game Night event type"""
     def test_open_game_night(self):
-        event7 = Event(5, "Labrador Nintendogs Caretakers", "Open Game Night", "Nintendogs", 50, datetime(2026, 3, 30))
-        self.assertEqual(event7.event_type, "Open Game Night")
+        event7 = Event(5, "Labrador Nintendogs Caretakers", EventType.OPEN_GAME_NIGHT, "Nintendogs", 50, datetime(2026, 3, 30))
+        self.assertEqual(event7.event_type, EventType.OPEN_GAME_NIGHT)
 
     """Test valid Ranked League Match event type"""
     def test_ranked_league_match(self):
-        event8 = Event(6, "Super Samsh Bros Ultimate Finals", "Ranked League Match", "Super Smash Bros Ultimate", 2, datetime(2024, 10, 9))
-        self.assertEqual(event8.event_type, "Ranked League Match")
+        event8 = Event(6, "Super Samsh Bros Ultimate Finals", EventType.RANKED_LEAGUE_MATCH, "Super Smash Bros Ultimate", 2, datetime(2024, 10, 9))
+        self.assertEqual(event8.event_type, EventType.RANKED_LEAGUE_MATCH)
 
     """Test valid Tournament Match"""
     def test_tournament_match(self):
-        event9 = Event(7, "Mortal Komabt 3: Quarter Finals", "Tournament Match", "Mortal Kombat 3", 8, datetime(2026, 5, 22))
+        event9 = Event(7, "Mortal Komabt 3: Quarter Finals", EventType.TOURNAMENT_MATCH, "Mortal Kombat 3", 8, datetime(2026, 5, 22))
+        self.assertEqual(event9.event_type, EventType.TOURNAMENT_MATCH)
 
     """Test invalid event type"""
     def test_invalid_event_type(self):
@@ -65,39 +66,39 @@ class TestEvent(unittest.TestCase):
     """Test invalid Game Title"""
     def test_invalid_game_title(self):
         with self.assertRaises(ValueError):
-            event11 = Event(9, "Ascension Zombies Tournament", "Open Game Night", "", 75, datetime(2025, 8, 17))
+            event11 = Event(9, "Ascension Zombies Tournament", EventType.OPEN_GAME_NIGHT, "", 75, datetime(2025, 8, 17))
     
     """Test valid Game Title"""
     def test_valid_game_title(self):
-        event12 = Event(10, "Plants vs zombies: TDM", "Tournament Match", "Plants vs. Zombies", 80, datetime(2025, 5, 4))
+        event12 = Event(10, "Plants vs zombies: TDM", EventType.TOURNAMENT_MATCH, "Plants vs. Zombies", 80, datetime(2025, 5, 4))
         self.assertEqual(event12.game_title, "Plants vs. Zombies")
 
     """Test invalid event date format"""
     def test_invalid_event_date(self):
         with self.assertRaises(ValueError):
-            event13 = Event(11, "Minecraft: speedrun of the ender Dragon Quest", "Ranked League Match", "Minecraft", 100, "2026-07-09")
+            event13 = Event(11, "Minecraft: speedrun of the ender Dragon Quest", EventType.RANKED_LEAGUE_MATCH, "Minecraft", 100, "2026-07-09")
 
     """Test valid event date format"""
     def test_valid_event_date(self):
-        event14 = Event(12, "Grand Theft Auto V: Hide and Seek", "Open Game Night", "Grand Theft Auto V", 90, datetime(2026, 6, 11))
+        event14 = Event(12, "Grand Theft Auto V: Hide and Seek", EventType.OPEN_GAME_NIGHT, "Grand Theft Auto V", 90, datetime(2026, 6, 11))
         self.assertEqual(event14.event_date, datetime(2026, 6, 11))
 
     """Test 1 game capacity"""
     def test_1_game_capacity(self):
-        event15 = Event(13, "Mario Kart Mushroom Gorge: Shroomless", "Tournament Match", "Mario Kart Wii", 1, datetime(2025, 11, 14))
+        event15 = Event(13, "Mario Kart Mushroom Gorge: Shroomless", EventType.RANKED_LEAGUE_MATCH, "Mario Kart Wii", 1, datetime(2025, 11, 14))
         self.assertEqual(event15.event_capacity, 1)
 
     """Test 100 game capacity"""
     def test_100_game_capacity(self):
-        event16 = Event(14, "Mario Kart Night", "Open Game Night", "Mario Kart", 100, datetime(2024, 1, 24))
+        event16 = Event(14, "Mario Kart Night", EventType.OPEN_GAME_NIGHT, "Mario Kart", 100, datetime(2024, 1, 24))
         self.assertEqual(event16.event_capacity, 100)
 
     """Test invalid game capacity"""
     def test_invalid_game_capacity(self):
         with self.assertRaises(ValueError):
-            event17 = Event(15, "Pac-Man 99 Speedrun", "Ranked League Match", "Pac-Man", -200, datetime(2026, 4, 25))
+            event17 = Event(15, "Pac-Man 99 Speedrun", EventType.RANKED_LEAGUE_MATCH, "Pac-Man", -200, datetime(2026, 4, 25))
 
     """Test valid game capacity"""
     def test_valid_game_capacity(self):
-        event18 = Event(16, "Fortnite: TDM", "Tournament Match", "Fortnite", 65, datetime(2024, 9, 19))
+        event18 = Event(16, "Fortnite: TDM", EventType.TOURNAMENT_MATCH, "Fortnite", 65, datetime(2024, 9, 19))
         self.assertEqual(event18.event_capacity, 65)
