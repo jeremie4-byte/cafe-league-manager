81d0b9b7013cd1fe49d6aff44101d8cb558249ce
jeremie4-byte <lortiejeremie@gmail.com>
2026-07-27 21:01:34 +0200

    Refactor Player class initialization with validators
    Validator functions added to domain logic

--- domains/player.py
+++ domains/player.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from datetime import datetime
+from services.validators import *
 
 """Creating an Enum class for all different SQL player_type"""
 class PlayerType(Enum):
@@ -10,20 +11,10 @@ class PlayerType(Enum):
 """Creating Player class"""
 class Player:
     def __init__(self, player_id, player_name, player_type: PlayerType, join_date: datetime, current_elo):
-        self.player_id = player_id
-        if player_id < 0:
-            raise ValueError
-        self.player_name = player_name
-        if player_name == "":
-            raise ValueError
-        self.player_type = player_type
+        self.player_id = validate_positive_id(player_id)
+        self.player_name = validate_min_name_length(player_name)
         if not isinstance(player_type, PlayerType):
-            raise ValueError
-        self.join_date = join_date
-        # We verify that join_date was actually entered as datetime format, if not we raise a value error
-        if not isinstance(join_date, datetime):
-            raise ValueError
-        self.current_elo = current_elo
-        if current_elo < 0 or current_elo > 4000:
-            raise ValueError
-
+            raise TypeError("player_type must be a valid PlayerType enum instance")
+        self.player_type = player_type
+        self.join_date = validate_datetime_format(join_date)
+        self.current_elo = validate_elo_range(current_elo)
