2dca3eee8b20e8b205076339121cf4b4a1187e8e
jeremie4-byte <lortiejeremie@gmail.com>
2026-07-28 22:05:52 +0200

    Refactor MatchPlayer tests for attendance and errors
    Updated attendance values for multiple MatchPlayer instances and changed exception types for invalid match results and attendance entries.

--- tests/test_match_player.py
+++ tests/test_match_player.py
@@ -16,18 +16,18 @@ class TestMatchPlayer (unittest.TestCase):
 
     """Test to see that 0 is an accepted player id value"""
     def test_valid_0player_id(self):
-        match_player2 = MatchPlayer(0, 2, MatchResult.SECOND_PLACE, Attendance.NO_SHOW)
+        match_player2 = MatchPlayer(0, 2, MatchResult.SECOND_PLACE, Attendance.ATTENDED)
         self.assertEqual(match_player2.player_id, 0)
 
     """Test to see that valid player_id are accepted"""
     def test_valid_player_id(self):
-        match_player3 = MatchPlayer(2, 3, MatchResult.THIRD_PLACE, Attendance.CANCELLED)
+        match_player3 = MatchPlayer(2, 3, MatchResult.THIRD_PLACE, Attendance.ATTENDED)
         self.assertEqual(match_player3.player_id, 2)
 
     """Test invalid player id raises a ValueError"""
     def test_invalid_player_id(self):
         with self.assertRaises(ValueError):
-            match_player4 = MatchPlayer(-500, 4, MatchResult.FOURTH_PLACE, Attendance.NO_SHOW)
+            match_player4 = MatchPlayer(-500, 4, MatchResult.FOURTH_PLACE, Attendance.ATTENDED)
 
     """Test to see that 0 is an accepted match id value"""
     def test_valid_0match_id(self):
@@ -46,7 +46,7 @@ class TestMatchPlayer (unittest.TestCase):
 
     """Test that match result FIRST_PLACE is accepted"""
     def test_first_place(self):
-        match_player8 = MatchPlayer(6, 6, MatchResult.FIRST_PLACE, Attendance.NO_SHOW)
+        match_player8 = MatchPlayer(6, 6, MatchResult.FIRST_PLACE, Attendance.ATTENDED)
         self.assertEqual(match_player8.match_result, MatchResult.FIRST_PLACE)
 
     """Test that match result SECOND_PLACE is accepted"""
@@ -56,7 +56,7 @@ class TestMatchPlayer (unittest.TestCase):
 
     """Test that match result THIRD_PLACE is accepted"""
     def test_third_place(self):
-        match_player10 = MatchPlayer(8, 8, MatchResult.THIRD_PLACE, Attendance.NO_SHOW)
+        match_player10 = MatchPlayer(8, 8, MatchResult.THIRD_PLACE, Attendance.ATTENDED)
         self.assertEqual(match_player10.match_result, MatchResult.THIRD_PLACE)
 
     """Test that match result FOURTH_PLACE is accepted"""
@@ -76,7 +76,7 @@ class TestMatchPlayer (unittest.TestCase):
 
     """Test that match result SEVENTH_PLACE is accepted"""
     def test_seventh_place(self):
-        match_player14 = MatchPlayer(12, 12, MatchResult.SEVENTH_PLACE, Attendance.CANCELLED)
+        match_player14 = MatchPlayer(12, 12, MatchResult.SEVENTH_PLACE, Attendance.ATTENDED)
         self.assertEqual(match_player14.match_result, MatchResult.SEVENTH_PLACE)
 
     """Test that match result EIGHT_PLACE is accepted"""
@@ -84,9 +84,9 @@ class TestMatchPlayer (unittest.TestCase):
         match_player15 = MatchPlayer(13, 13, MatchResult.EIGHT_PLACE, Attendance.ATTENDED)
         self.assertEqual(match_player15.match_result, MatchResult.EIGHT_PLACE)
 
-    """Test that an invalid match result raises a ValueError"""
+    """Test that an invalid match result raises an unvalid TypeError"""
     def test_invalid_place(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(TypeError):
             match_player16 = MatchPlayer(14, 14, "9th Place", Attendance.NO_SHOW)
 
     """Test that attendance value 'Attended' is accepted"""
@@ -96,15 +96,15 @@ class TestMatchPlayer (unittest.TestCase):
 
     """Test that attendance value 'Cancelled' is accepted"""
     def test_match_cancelled(self):
-        match_player18 = MatchPlayer(16, 16, MatchResult.EIGHT_PLACE, Attendance.CANCELLED)
+        match_player18 = MatchPlayer(16, 16, MatchResult.NONE, Attendance.CANCELLED)
         self.assertEqual(match_player18.attendance, Attendance.CANCELLED)
 
     """Test that attendance value 'No Show' is accepted"""
     def test_match_no_show(self):
-        match_player19 = MatchPlayer(17, 17, MatchResult.FOURTH_PLACE, Attendance.NO_SHOW)
+        match_player19 = MatchPlayer(17, 17, MatchResult.NONE, Attendance.NO_SHOW)
         self.assertEqual(match_player19.attendance, Attendance.NO_SHOW)
 
-    """Test that an invalid attendance entry raises a ValueError"""
+    """Test that an invalid attendance entry raises a TypeeError"""
     def test_invalid_match_attendance(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(TypeError):
             match_player20 = MatchPlayer(18, 18, MatchResult.SEVENTH_PLACE, 'Postponed')
